6b1519609d5a34973b1837d2b84a97654974f291
SAQUIB KHAN <saquibkhan890098@gmail.com>
2025-01-15 15:48:55 +0530

    updated main.jsx

--- frontend/src/reportWebVitals.js
+++ frontend/src/reportWebVitals.js
@@ -2,6 +2,7 @@ import { getCLS, getFID, getFCP, getLCP, getTTFB } from "web-vitals";
 
 const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
+    console.log("Reporting Web Vitals...");
     getCLS(onPerfEntry);
     getFID(onPerfEntry);
     getFCP(onPerfEntry);
